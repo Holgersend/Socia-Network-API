1f3d3a5025d5f9d0bddc8cc8f67907f18b69d742
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-10 21:18:13 -0500

    Update userController.js
    created getUsers function to fetch all users from the database

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,22 @@ const { User } = require('../models');
 
 
 module.exports = {
-// GET all users 
+ // GET all users 
+    async getUsers(req, res){
+        try {
+            const users = await User.find();
+
+            const userObj = {
+                users,
+                headCount: await headCount(),
+            };
+
+            res.json(userObj);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 // GET a single user by its id
 // POST a new user
 // PUT to update a user by its id 
