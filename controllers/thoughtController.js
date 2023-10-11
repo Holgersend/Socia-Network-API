537cfe7445d6c3ebcc43ef9952194396b7e4501c
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-10 21:01:09 -0500

    controllers
    created controllers structure for user and thoght

---
+++ controllers/thoughtController.js
@@ -0,0 +1,10 @@
+const { Thought } = require('../models');
+
+module.exports = {
+// GET all thoughts
+// GET to get a single thought by its id 
+// POST to create a new thought
+// PUT  to update a thought bt its id
+// DELETE to remove a thought by its id
+
+};
---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const { User } = require('../models');
+
+
+module.exports = {
+// GET all users 
+// GET a single user by its id
+// POST a new user
+// PUT to update a user by its id 
+// DELETE to remove user by its id
+};
