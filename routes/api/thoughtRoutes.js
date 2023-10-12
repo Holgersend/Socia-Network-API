3351779c746b26b9564aa4c714ee9c623b24d8dd
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-11 23:35:57 -0500

    Create thoughtRoutes.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,15 @@
+const thoughtController = require('../../controllers/thoughtController');
+
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('./:thoughtId').get(getSingleThought);
+
+module.exports = router;
